daa55398b1ba9960bf1ae7a902690a5c6e83b0f8
Cicero Augusto <cicero1995augustoo@gmail.com>
2024-12-13 17:12:33 -0300

    Logging configured

---
+++ src/main/java/com/example/vamojossa/utils/LoggerManager.java
@@ -0,0 +1,19 @@
+package com.example.vamojossa.utils;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class LoggerManager {
+
+        private static final Logger logger = LogManager.getLogger();
+
+        public static void info(String message) {
+
+                logger.info(message);
+        }
+
+        public static void error(String message) {
+
+                logger.error(message);
+        }
+}
