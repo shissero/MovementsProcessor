d8df5cbf8e9002d7f15ca51b0a5a82a3a7781527
Cicero Augusto <cicero1995augustoo@gmail.com>
2024-12-24 21:30:42 -0300

    Implemented dynamic creation of table columns in TableDataDisplayer

--- src/main/java/com/example/vamojossa/Main.java
+++ src/main/java/com/example/vamojossa/Main.java
@@ -1,7 +1,11 @@
 package com.example.vamojossa;
 
+import com.example.vamojossa.models.movement.getdatastratagies.NubankCSVStrategy;
+import com.example.vamojossa.presenters.TableDataPresenter;
 import javafx.application.Application;
 import javafx.application.Platform;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
@@ -15,14 +19,14 @@ public class Main extends Application {
         @Override
         public void start(Stage stage) throws IOException {
 
-                //ObservableList<TableDataPresenter.TableDataAdapter> all_movements = FXCollections.observableArrayList(NubankCSVStrategy.fromCSV());
+                ObservableList<? extends TableDataPresenter.TableDataAdapter> all_movements = (new NubankCSVStrategy()).getMovements("src/main/resources/com/example/vamojossa/test_ignore");
 
                 FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("movements_table.fxml"));
 
                 Scene scene = new Scene(fxmlLoader.load());
                 scene.getStylesheets().add(Main.class.getResource("movement_table_style.css").toExternalForm());
 
-                //TableDataPresenter presenter = new TableDataPresenter(all_movements, fxmlLoader.getController());
+                TableDataPresenter presenter = new TableDataPresenter(all_movements, fxmlLoader.getController());
 
                 stage.setTitle("Hello!");
                 stage.setScene(scene);
@@ -31,8 +35,6 @@ public class Main extends Application {
         }
 
         public static void main(String[] args) {
-
-                Platform.exit();
 
                 launch();
         }
--- src/main/java/com/example/vamojossa/views/MoneyMovementView.java
+++
@@ -1,8 +0,0 @@
-package com.example.vamojossa.views;
-
-public interface MoneyMovementView {
-
-        void updateDescription(String description);
-        void updateDate(String date);
-        void updateAmount(String amount);
-}
