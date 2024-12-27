4f9e5b06e6e964c91096cea6555f5e60b3285d22
Cicero Augusto <cicero1995augustoo@gmail.com>
2024-12-27 12:17:44 -0300

    Minor changes

--- src/main/java/com/example/vamojossa/controllers/MovementEditorController.java
+++ src/main/java/com/example/vamojossa/controllers/MovementEditorController.java
@@ -3,7 +3,7 @@ package com.example.vamojossa.controllers;
 import com.example.vamojossa.models.movement.Movement;
 import javafx.scene.control.TableView;
 
-public class NubankDataProcessingController {
+public class MovementEditorController {
 
         TableView<String> view = new TableView<>();
         Movement movement;
